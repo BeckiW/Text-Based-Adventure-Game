c61176b5a16639274ca4c9fc0ce11c6f3741ec3a
Becki Wordsworth <r.a.wordsworth@gmail.com>
2020-01-02 00:51:45 +0800

    Implementing the game

---
+++ Text based game/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] state startingState;
+
+
+    state state;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+        
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.getNextStates();
+
+        for (int index = 0; index < nextStates.Length; index++ )
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[0];
+            }
+
+        }
+
+    }
+}
---
+++ Text based game/Assets/Scripts/state.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+
+public class state : ScriptableObject
+{
+   [TextArea(14, 10)] [SerializeField] string storyText;
+
+    [SerializeField] state[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public state[] getNextStates()
+    {
+        return nextStates;
+    }
+}
